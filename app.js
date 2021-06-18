6b7c04eb1c09b6ff83bed66327b35abe0eb7d96c
Nicolas Cabrera Lettiere <nicolas.cabrera.lettiere@gmail.com>
2021-06-18 00:13:29 -0300

    Webhooks test7

--- app.js
+++ app.js
@@ -65,27 +65,12 @@ app.get('/detail/ckeckout/failure', function (req, res) {
 //});
 
 app.post('/notifications',  (request, response) => {
-    const event = request.body;
+    const event = request.query;
 
     console.log("*************  [  -EVENT-  ]  ***************");
     console.log(event);
     console.log("*************  [  -END-  ]  ***************");
     // Handle the event
-    switch (event.type) {
-      case 'payment_intent.succeeded':
-        const paymentIntent = event.data.object;
-        // Then define and call a method to handle the successful payment intent.
-        // handlePaymentIntentSucceeded(paymentIntent);
-        break;
-      case 'payment_method.attached':
-        const paymentMethod = event.data.object;
-        // Then define and call a method to handle the successful attachment of a PaymentMethod.
-        // handlePaymentMethodAttached(paymentMethod);
-        break;
-      // ... handle other event types
-      default:
-        console.log(`Unhandled event type ${event.type}`);
-    }
   
     // Return a response to acknowledge receipt of the event
     //response.json({received: true});
