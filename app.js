fb228a0e33d29314a9d64ef36e58b393069fc62d
Nicolas Cabrera Lettiere <nicolas.cabrera.lettiere@gmail.com>
2021-06-18 00:07:24 -0300

    Webhooks test5

--- app.js
+++ app.js
@@ -50,18 +50,46 @@ app.get('/detail/ckeckout/failure', function (req, res) {
     res.render('failure', req.query);
 });
 
-app.post("/notifications", function (req, res) {
-    console.log("************* [ WebHook ]  ***************");
-    console.log("************* [ req.body ] ***************");
-    console.log(req.body);
-    console.log("************* [ res.body ] ***************");
-    console.log(res.body);
-    console.log("************* [ req.query ] ***************");
-    console.log(req.query);
-    console.log("************* [ res.query ] ***************");
-    console.log(res.query);
+//app.post("/notifications", function (req, res) {
+//    console.log("************* [ WebHook ]  ***************");
+//    console.log("************* [ req.body ] ***************");
+//    console.log(req.body);
+//    console.log("************* [ res.body ] ***************");
+//    console.log(res.body);
+//    console.log("************* [ req.query ] ***************");
+//    console.log(req.query);
+//    console.log("************* [ res.query ] ***************");
+//    console.log(res.query);
+//    console.log("*************  [  -END-  ]  ***************");
+//    res.status(200).send("OK");
+//});
+
+app.post('/notifications', bodyParser.raw({type: 'application/json'}), (request, response) => {
+    const event = request.body;
+
+    console.log("*************  [  -EVENT-  ]  ***************");
+    console.log(event);
     console.log("*************  [  -END-  ]  ***************");
-    res.status(201).send("OK");
-});
+    // Handle the event
+    switch (event.type) {
+      case 'payment_intent.succeeded':
+        const paymentIntent = event.data.object;
+        // Then define and call a method to handle the successful payment intent.
+        // handlePaymentIntentSucceeded(paymentIntent);
+        break;
+      case 'payment_method.attached':
+        const paymentMethod = event.data.object;
+        // Then define and call a method to handle the successful attachment of a PaymentMethod.
+        // handlePaymentMethodAttached(paymentMethod);
+        break;
+      // ... handle other event types
+      default:
+        console.log(`Unhandled event type ${event.type}`);
+    }
+  
+    // Return a response to acknowledge receipt of the event
+    //response.json({received: true});
+    response.status(200).send("OK");
+  });
 
 app.listen(port);
